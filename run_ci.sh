edcfc788c2ae6caa29d6901daa0d4a03a5209055
Sam Messina <sam.messina@gmail.com>
2014-03-23 23:13:08 -0700

    Working on CI file

--- run_ci.sh
+++ run_ci.sh
@@ -6,8 +6,12 @@
 #   Run this script normally and the latest code will be synced in ./informant/
 #   To set as startup on raspberry pi: run_ci --install
 
+# Spec: Running this file normally copies its directory to tmp and runs from there by
+#       using the args: run <origindir>
+
 # local vars
 localdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #http://stackoverflow.com/a/246128
+synceddir=""
 this_script_hash=`md5sum ${localdir}/run_ci.sh`
 
 # Set as startup program on raspberry pi
@@ -34,7 +38,7 @@ function stop_informant {
 
 function query_git {
     echo "querying remote repo"
-    cd ${localdir}
+    cd ${synceddir}
     [ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ] && changed=0 || changed=1 #http://stackoverflow.com/a/16920556
     echo "git changed=${changed}"
     if [ ${changed} -eq 1 ]; then
@@ -46,29 +50,43 @@ function update {
     echo "updating informant"
     stop_informant
     #git pull
-    cd ${localdir}
+    cd ${synceddir}
     git fetch --all
     git reset --hard origin/master
-    chmod +x ${localdir}/run_ci.sh
-    new_script_hash=`md5sum ${localdir}/run_ci.sh`
+    chmod +x ${synceddir}/run_ci.sh
+    new_script_hash=`md5sum ${synceddir}/run_ci.sh`
     echo "new hash = $new_script_hash, old hash = $this_script_hash"
     if [ "$new_script_hash" != "$this_script_hash" ]; then
         echo "CI script updated, restarting..."
-        ${localdir}/run_ci.sh &
+        ${synceddir}/run_ci.sh &
         exit 0
     fi
     start_informant
 }
 
-stop_informant
-update
-if [ $(pidof -x run_ci.sh | wc -w) -gt 2 ]; then
-    echo "killing other run_ci process"
-    kill `pidof -x -o $$ run_ci.sh`
-fi
-start_informant
+function run {
+    synceddir="$2"
+    stop_informant
+    update
+    if [ $(pidof -x run_ci.sh | wc -w) -gt 2 ]; then
+        echo "killing other run_ci process"
+        kill `pidof -x -o $$ run_ci.sh`
+    fi
+    start_informant
 
-while true; do
-    query_git
-    sleep 30
-done
+    while true; do
+        query_git
+        sleep 30
+    done
+}
+
+if [ "$1" == "run" ]; then
+    run
+    exit 0
+fi
+
+# copy over to tmp to run
+# this is because run_ci.sh was being zeroed
+tmpdir=`mktemp -d`
+cp . ${tmpdir}
+${tmpdir}/run_ci.sh run ${localdir}
