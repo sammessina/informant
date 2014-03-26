ecfea720107cf7e008f113f9c2dd151e15fb63a5
Sam Messina <sam.messina@gmail.com>
2014-03-26 01:22:34 -0700

    Raspberry pi setup instructions

--- src/modules/weather.py
+++ src/modules/weather.py
@@ -15,10 +15,12 @@ class Module():
 class WeatherModule(Module):
     def __init__(self):
         Module.__init__(self)
-        url = 'http://api.wunderground.com/api/c5cec5a481c71293/conditions/q/98052.json'
-        result = json.load(urllib.urlopen(url))
-        self.temp_f = result['current_observation']['temp_f']
         self.label = render.TextImg(color="blue")
+        self.temp_f = 999
+        #url = 'http://api.wunderground.com/api/c5cec5a481c71293/conditions/q/98052.json'
+        #result = json.load(urllib.urlopen(url))
+        #self.temp_f = result['current_observation']['temp_f']
+        #
 
     def render(self, screen):
         self.label.render(screen, 500, 500, "%.1f F" % self.temp_f)
