0b572574c980975e44625520ccfcb6f298473fd1
Sam Messina <sam.messina@gmail.com>
2014-03-26 19:51:13 -0700

    Raspberry pi setup instructions

--- run_ci.sh
+++ run_ci.sh
@@ -42,6 +42,7 @@ function stop_informant {
 function query_git {
     echo "querying remote repo"
     cd ${localdir}
+    changed=0
     log="`git ls-remote origin -h refs/heads/master | cut -f1`"
     if [ ${#log} -eq 40 ]; then
         [ "$log" = "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" ] && changed=0 || changed=1 #http://stackoverflow.com/a/16920556
